a5df1eb335aabb4adb4dc4e22337bf52e02cc10d
Harry7986 <emarh.harry.code@gmail.com>
2025-10-04 05:33:34 +0200

    Slider jsx file 3

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import leftChevron from "../../assets/left-arrow.svg";
+import rightChevron from "../../assets/right-arrow.svg";
 
 const Slider = () => {
   return (
-    <div>Slider</div>
+    <>
+        <p className="index-info">3 / 5</p>
+        <div className="slider">
+            <p className="image-info">Bedroom</p>
+            <img src="/public/images/img-3.jpg" alt="Bedroom" className="slider-image" />
+
+            <button className="navigation-button prev-button"><img src={leftChevron} alt='left' /></button>
+            <button className="navigation-button next-button"><img src={rightChevron} alt='right' /></button>
+        </div>
+    </>
   )
 }
 
