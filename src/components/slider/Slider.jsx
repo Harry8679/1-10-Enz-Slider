e47895b5abbf362ec724b8e3dca900d9cc9f6fe0
Harry7986 <emarh.harry.code@gmail.com>
2025-10-18 12:38:07 +0200

    Prev and Next Slide

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,17 +1,29 @@
+import { useState } from 'react';
 import './Slider.css';
 import leftChevron from "../../assets/left-arrow.svg";
 import rightChevron from "../../assets/right-arrow.svg";
+import sliderData from '../../data/sliderData';
 
 const Slider = () => {
+  const [slideIndex, setSliderIndex] = useState(1);
+  const currentSlide = sliderData.find(object => object.id === slideIndex);
+
+  const nextSlide = () => setSliderIndex(prev => prev === sliderData.length ? 1 : prev + 1);
+  const prevSlide = () => setSliderIndex(prev => prev === 1 ? sliderData.length : prev - 1);
+
   return (
     <>
-        <p className="index-info">3 / 5</p>
+        <p className="index-info">{slideIndex} / {sliderData.length}</p>
         <div className="slider">
-            <p className="image-info">Bedroom</p>
-            <img src="/public/images/img-3.jpg" alt="Bedroom" className="slider-image" />
+            <p className="image-info">{currentSlide.description}</p>
+            <img src={`/public/images/img-${slideIndex}.jpg`} alt="Bedroom" className="slider-image" />
 
-            <button className="navigation-button prev-button"><img src={leftChevron} alt='left' /></button>
-            <button className="navigation-button next-button"><img src={rightChevron} alt='right' /></button>
+            <button className="navigation-button prev-button">
+                <img src={leftChevron} alt='left' />
+            </button>
+            <button className="navigation-button next-button">
+                <img src={rightChevron} alt='right' />
+            </button>
         </div>
     </>
   )
